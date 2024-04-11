085a83cb77520fc5d4337eb0d9abb797f4961cdd
umassdgithub <amir.umassd@gmail.com>
2024-04-11 08:04:09 -0400

    workflow

--- index.js
+++ index.js
@@ -7,7 +7,4 @@ app.get('/', (req, res) => {
     res.send('Hello World -- this is from actions!')
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-
 module.exports = app;
