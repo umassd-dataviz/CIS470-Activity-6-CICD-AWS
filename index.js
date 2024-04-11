988fc3dafca477259d1ae2f694ecd45bcf758259
umassdgithub <amir.umassd@gmail.com>
2024-04-11 07:53:48 -0400

    workflow

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
+    res.send('Hello World -- this is from actions!')
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
