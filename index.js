e720856186ed9dbebaae3cf793fa98cd5eaa098e
umassdgithub <amir.umassd@gmail.com>
2024-04-11 08:35:34 -0400

    workflow

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const serverless = require('serverless-http');
 
 const app = express();
-
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello from Express!' });
+app.use(express.static('public'));
+app.get('/default/CIS470-Activity-8', (req, res) => {
+    //res.json({ message: 'Hello from Express!' });
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 module.exports.handler = serverless(app);
