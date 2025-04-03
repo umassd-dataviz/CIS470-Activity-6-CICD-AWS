04e47dbc7fb722077b4534892c167e5e9df78522
rjcallaghan <rcallaghan@umassd.edu>
2025-04-03 11:41:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 app.get('/default/CIS470-Activity-6', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
-
+// test
 app.post('/default/CIS470-Activity-6/check-triangle', (req, res) => {
     const { side1, side2, side3 } = req.body;
     const result = classifyTriangle(side1,side2,side3);
